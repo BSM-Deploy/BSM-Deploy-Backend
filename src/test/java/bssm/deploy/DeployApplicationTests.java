26b85f92527a1977eea30c602121866efec6cc2f
leehj050211 <leehj050211@gmail.com>
2023-03-08 09:15:49 +0900

    Initial commit

---
+++ src/test/java/bssm/deploy/DeployApplicationTests.java
@@ -0,0 +1,13 @@
+package bssm.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
