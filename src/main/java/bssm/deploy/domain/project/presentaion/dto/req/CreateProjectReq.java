e1357643b97fe1a3f898a0ed6246a5b77d576cfc
leehj050211 <leehj050211@gmail.com>
2023-04-20 10:26:44 +0900

    FIX: 도메인 접두사에 서브 도메인을 사용할 수 없도록 변경

--- src/main/java/bssm/deploy/domain/project/presentaion/dto/req/CreateProjectReq.java
+++ src/main/java/bssm/deploy/domain/project/presentaion/dto/req/CreateProjectReq.java
@@ -4,16 +4,19 @@ import bssm.deploy.domain.project.domain.type.ProjectType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 public class CreateProjectReq {
 
     @Schema(description = "프로젝트 이름")
     @NotBlank
+    @Length(min = 2, max = 16)
     private String name;
 
     @Schema(description = "도메인 접두사", defaultValue = "string")
-    @Pattern(regexp = "^[a-zA-Z0-9]+([-.][a-zA-Z0-9]+)*$")
+    @Pattern(regexp = "^[a-zA-Z0-9-]+$")
+    @Length(min = 3, max = 63)
     private String domainPrefix;
 
     @Schema(description = "프로젝트 파일 형식")
